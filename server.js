03d72741246bc065e401e8474d5922bb86ec7699
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-03-23 12:45:22 -0500

    GET routes added using Express Router

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
@@ -9,14 +12,18 @@ const db = require('./config/keys').mongoURI;
 
 // connect to mongoDB through mongoose
 // this will use a promise and .then 
-mongoose.connect(db)
+mongoose.connect(db, { useNewUrlParser: true })
    // success
    .then(() => console.log('connection to db is successful')) 
    // failure to connect
    .catch(err => console.log(err)); // failure
 
-
 app.get('/', (req, res) => res.send('Hello')); 
+
+// Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 4000;
 
